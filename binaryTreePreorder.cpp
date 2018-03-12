0459a7cd20fc44d00bff86e4ce07d1dc0a5c3aec
pramnegi <negi.1984@gmail.com>
2018-03-12 09:00:28 -0700

    Update binaryTreePreorder.cpp

--- binaryTreePreorder.cpp
+++ binaryTreePreorder.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-class binary_tree{
+class binary_tree {
 public:
     class node{
         public:
         int value;
-        node *left;
-        node *right;
+        node* left;
+        node* right;
     };
     typedef node* nodeptr;
     nodeptr root;
@@ -20,49 +20,45 @@ public:
 
 };
 
-binary_tree::binary_tree(){
- root =NULL;
+binary_tree::binary_tree() {
+ root = NULL;
 }
 
-void binary_tree::binary_insert(){
+// I WOULD PREFER writing a insert function, assuming it is a binary search tree. 
+// that will make your life simple
+void binary_tree::binary_insert() {
     root=new node;
-    root->value=1;
-    root->left=new node;
-    root->right=new node;
-    root->left->value=2;
-    root->right->value=3;
-    root->left->left=new node;
-    root->left->left->value=4;
-    root->left->left->left=NULL;
-    root->left->left->right=NULL;
-    root->right->left=NULL;
-    root->right->right=NULL;
+    root->value = 1;
+    root->left = new node;
+    root->right = new node;
+    root->left->value = 2;
+    root->right->value = 3;
+    root->left->left = new node;
+    root->left->left->value = 4;
+    root->left->left->left = NULL;
+    root->left->left->right = NULL;
+    root->right->left = NULL;
+    root->right->right = NULL;
 }
 
-void binary_tree::preorder_recursive_wrapper(){
-    if (root==NULL)
-    {
+void binary_tree::preorder_recursive_wrapper() {
+    if (root == NULL) {
         cout<<"tree is empty";
         return;
     }
-    else
-    {
-       nodeptr temp=root;
-       preorder_recursive(temp);
-    }
+    nodeptr temp = root;
+    preorder_recursive(temp);
 }
 
-void binary_tree::preorder_recursive(nodeptr root){
-    if (root!=NULL)
-    {
+void binary_tree::preorder_recursive(nodeptr root) {
+    if (root != NULL) {
         cout<<root->value;
         preorder_recursive(root->left);
         preorder_recursive(root->right);
     }
 }
 
-int main()
-{
+int main() {
     binary_tree b1;
     b1.binary_insert();
     b1.preorder_recursive_wrapper();
